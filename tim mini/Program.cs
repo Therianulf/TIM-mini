33968e5685182bce56cb268f7c15c925bbba6c7e
Benjamin Larson <therianlarson@gmail.com>
2018-10-30 13:26:22 -0700

    updating version, and using mdk readme for comments

--- tim mini/Program.cs
+++ tim mini/Program.cs
@@ -19,28 +19,6 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        /*
-     Taleden's Inventory Manager
-     version 1.7.0 (2018-10-30)
-
-     "There are some who call me... TIM?"
-
-     Steam Workshop: http://steamcommunity.com/sharedfiles/filedetails/?id=546825757
-     User's Guide:   http://steamcommunity.com/sharedfiles/filedetails/?id=546909551
-
-
-     **********************
-     ADVANCED CONFIGURATION
-
-     The settings below may be changed if you like, but read the notes and remember
-     that any changes will be reverted when you update the script from the workshop.
-     */
-
-        // Each "Type/" section can have multiple "/Subtype"s, which are formatted like
-        // "/Subtype,MinQta,PctQta,Label,Blueprint". Label and Blueprint specified only
-        // if different from Subtype, but Ingot and Ore have no Blueprint. Quota values
-        // are based on material requirements for various blueprints (some built in to
-        // the game, some from the community workshop).
         const string DEFAULT_ITEMS = @"
 AmmoMagazine/
 /Missile200mm
--- tim mini/Readme.cs
+++ tim mini/Readme.cs
@@ -2,7 +2,32 @@
  *   R e a d m e
  *   -----------
  * 
- *   Use https://github.com/malware-dev/MDK-SE to minify
+ *   Uses https://github.com/malware-dev/MDK-SE to minify
+ *   
+ *   
+     Taleden's Inventory Manager
+     version 1.7.0 (2018-10-30)
+
+     Updated by Therian
+
+     "There are some who call me... TIM?"
+
+     Steam Workshop: http://steamcommunity.com/sharedfiles/filedetails/?id=546825757
+     User's Guide:   http://steamcommunity.com/sharedfiles/filedetails/?id=546909551
+
+
+     **********************
+     ADVANCED CONFIGURATION
+
+     The settings below may be changed if you like, but read the notes and remember
+     that any changes will be reverted when you update the script from the workshop.
+     
+
+         Each "Type/" section can have multiple "/Subtype"s, which are formatted like
+         "/Subtype,MinQta,PctQta,Label,Blueprint". Label and Blueprint specified only
+         if different from Subtype, but Ingot and Ore have no Blueprint. Quota values
+         are based on material requirements for various blueprints (some built in to
+         the game, some from the community workshop).
  *   
  * 
  */
